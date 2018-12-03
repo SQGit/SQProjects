c81bc58bf80c7944a4b6c266b21df46329e02769
guna06 <guna@sqindia.net>
2018-12-03 17:29:31 +0530

    Autoparts
    Autoparts for Mobile(03-12-18)
    Source Code

---
+++ ANDROID/Autoparts Mobile/app/src/main/java/com/sqindia/www/auto360parts/Activity/TransformationActivity.java
@@ -0,0 +1,54 @@
+package com.sqindia.www.auto360parts.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.sqindia.www.auto360parts.Fragment.upload.FirstFragment;
+import com.sqindia.www.auto360parts.Fragment.upload.FourthFragment;
+import com.sqindia.www.auto360parts.Fragment.upload.SecondFragment;
+import com.sqindia.www.auto360parts.Fragment.upload.ThirdFragment;
+import com.sqindia.www.auto360parts.R;
+import com.sqindia.www.auto360parts.Transformations.GateTransformation;
+import com.sqindia.www.auto360parts.Adapter.MyPagerAdapter;
+
+
+
+public class TransformationActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    MyPagerAdapter pagerAdapter;
+    Intent intent;
+    public static String get_car1,get_car2,get_vin1,get_vin2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transformation);
+
+        intent=getIntent();
+        get_car1=intent.getStringExtra("pull_car1");
+        get_car2=intent.getStringExtra("pull_car2");
+        get_vin1=intent.getStringExtra("pull_vin1");
+        get_vin2=intent.getStringExtra("pull_vin2");
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        addingFragmentsTOpagerAdapter();
+        viewPager.setAdapter(pagerAdapter);
+        GateTransformation gateTransformation = new GateTransformation();
+        viewPager.setPageTransformer(true, gateTransformation);
+    }
+
+
+    private void addingFragmentsTOpagerAdapter() {
+        pagerAdapter.addFragments(new FirstFragment());
+        pagerAdapter.addFragments(new SecondFragment());
+        pagerAdapter.addFragments(new ThirdFragment());
+        pagerAdapter.addFragments(new FourthFragment());
+    }
+
+}
